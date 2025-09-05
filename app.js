9110874b2fadda49a8f1e8997c41767f4c8b6da1
RB01622 <kbkannah@gmail.com>
2025-09-05 11:01:35 +0530

    Added package &b node.js - 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from my Node.js app running on GitHub Actions!");
+  res.send("Hello from Node app.js app running on GitHub Actions!");
 });
 
 const PORT = process.env.PORT || 3000;
